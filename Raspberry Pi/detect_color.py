ec1c4ed9ce9d9616824f32987b32eae2c7322443
Kaaiman <103824097+Kaaiman@users.noreply.github.com>
2022-10-05 17:03:11 +0200

    Delete Raspberry Pi directory

--- Raspberry Pi/detect_color.py
+++
@@ -1,8 +0,0 @@
-import os
-from convert_image import convert
-from rgbToColor import photo_to_color
-
-def detect_color():
-    os.system('libcamera-jpeg  -o photo.jpg --width 640 --height 480 --nopreview -t 1')
-    convert(0.2)
-    return photo_to_color()
