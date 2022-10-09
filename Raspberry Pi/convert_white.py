951c6e4ab4dceec1ad70f4ac9cf3aba22394d546
Kaaiman <kajbreeman@gmail.com>
2022-10-09 11:28:39 +0200

    convert update

--- Raspberry Pi/convert_white.py
+++ Raspberry Pi/convert_white.py
@@ -2,80 +2,81 @@ import os
 import cv2
 import numpy as np
 
-work_dir = os.path.join('e:', os.sep, 'Files', 'Code', 'Python', 'Projects', 'Lego_Sorter', 'misc')
-input_dir = os.path.join(work_dir, 'photo.jpg')
-output_dir = os.path.join(work_dir, 'output.jpg')
+def white_convert(border_percentage, noise_thresh):
+    input_dir = 'photo.jpg'
+    output_dir = '/home/legopi/Lego_Sorter/Raspberry Pi/Output/output.jpg'
 
-bgr_input = cv2.imread(input_dir)
-hsv_input = cv2.cvtColor(bgr_input, cv2.COLOR_BGR2HSV)
+    bgr_input = cv2.imread(input_dir)
+    hsv_input = cv2.cvtColor(bgr_input, cv2.COLOR_BGR2HSV)
 
-mask = cv2.inRange(hsv_input, np.array([0, 0, 0]), np.array([120, 255, 255]))
+    mask = cv2.inRange(hsv_input, np.array([0, 0, 0]), np.array([30, 255, 255]))
 
-cv2.imshow('mask', mask)
-cv2.waitKey(0)
+    cv2.imshow('mask', mask)
+    cv2.waitKey(0)
 
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-contour_img = bgr_input.copy()
+    contour_img = bgr_input.copy()
 
-cv2.drawContours(contour_img, contours, -1, (0,255,0), 3)
+    cv2.drawContours(contour_img, contours, -1, (0,255,0), 3)
 
-cv2.imshow('contours', contour_img)
-cv2.waitKey(0)
+    cv2.imshow('contours', contour_img)
+    cv2.waitKey(0)
 
-xList = []
-yList = []
-wList = []
-hList = []
+    xList = []
+    yList = []
+    wList = []
+    hList = []
 
 
-for i in contours:
-    x,y,w,h = cv2.boundingRect(i)
-    xList.append(x)
-    yList.append(y)
-    wList.append(w)
-    hList.append(h)
+    for i in contours:
+        x,y,w,h = cv2.boundingRect(i)
+        if w * h > noise_thresh: 
+            xList.append(x)
+            yList.append(y)
+            wList.append(w)
+            hList.append(h)
 
-xListMax = []
-yListMax = []
+    xListMax = []
+    yListMax = []
 
-xLength = len(xList) - 1
-yLength = len(yList) - 1
+    xLength = len(xList) - 1
+    yLength = len(yList) - 1
 
-i = 0
-while i <= xLength:
-    newX = xList[i] + wList[i]
-    xListMax.append(newX)
-    i = i + 1
-i = 0
-while i <= yLength:
-    newY = yList[i] + hList[i]
-    yListMax.append(newY)
-    i = i + 1
-    
-xList.sort()
-yList.sort()
-xListMax.sort()
-yListMax.sort()
+    i = 0
+    while i <= xLength:
+        newX = xList[i] + wList[i]
+        xListMax.append(newX)
+        i = i + 1
+    i = 0
+    while i <= yLength:
+        newY = yList[i] + hList[i]
+        yListMax.append(newY)
+        i = i + 1
+        
+    xList.sort()
+    yList.sort()
+    xListMax.sort()
+    yListMax.sort()
 
-x = xList[0]
-y = yList[0]
-xM = xListMax[-1]
-yM = yListMax[-1]
+    x = xList[0]
+    y = yList[0]
+    xM = xListMax[-1]
+    yM = yListMax[-1]
 
-offsetPercentage = 0
+    offsetPercentage = border_percentage
 
-xOffset = round((xM - x) * offsetPercentage)
-yOffset = round((yM - y) * offsetPercentage)
+    xOffset = round((xM - x) * offsetPercentage)
+    yOffset = round((yM - y) * offsetPercentage)
 
-x = x - xOffset
-y = y - yOffset
-xM = xM + xOffset
-yM = yM + yOffset
+    x = x - xOffset
+    y = y - yOffset
+    xM = xM + xOffset
+    yM = yM + yOffset
 
-img_crop = bgr_input[y:yM, x:xM]
+    img_crop = bgr_input[y:yM, x:xM]
 
-cv2.imshow('cropped', img_crop)
-cv2.waitKey(0)
+    cv2.imshow('cropped', img_crop)
+    cv2.waitKey(0)
 
-cv2.imwrite(output_dir, img_crop)
+    cv2.imwrite(output_dir, img_crop)
--- Raspberry Pi/detect_part.py
+++ Raspberry Pi/detect_part.py
@@ -1,8 +1,10 @@
 from convert_image import convert
 from predict import detect_part
+from convert_white import white_convert
 import os
 
 os.system('libcamera-jpeg  -o photo.jpg --width 640 --height 480 --nopreview -t 1')
-convert(0)
+#convert(0)
+white_convert(0, 0)
 part = detect_part()
 print(part)
